3be8d6875de788d9068586f6bad73089d368fb47
Apodemus Erectus <bnkho98@hotmail.com>
2021-08-26 10:42:52 +0200

    added entities

---
+++ TheMessage.Business/Context/MessageDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheMessage.Business.Entities;
+
+namespace TheMessage.Business.Context
+{
+    class MessageDataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public MessageDataContext(DbContextOptions<MessageDataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ TheMessage.Business/Interfaces/Entities/IUser.cs
@@ -0,0 +1,10 @@
+﻿namespace TheMessage.Business.Interfaces.Entities
+{
+    public interface IUser
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Lastname { get; set; }
+        string Email { get; set; }
+    }
+}
